0184212cfb57e4ec085c345ae5561b06366151c3
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-09-22 19:04:34 +0530

    ajax request for add to cart

--- index.php
+++ index.php
@@ -38,6 +38,7 @@
 </nav>
 
 <div class="container">
+    <div id="message"></div>
     <div class="row mt-2 pb-3">
         <?php  
             include 'config.php';
@@ -82,6 +83,39 @@
 
 <!-- Latest compiled JavaScript -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
+<script>
+    $(document).ready(function(){
+        $(".addItemBtn").click(function(e){
+            e.preventDefault();
+
+            var $form = $(this).closest(".form-submit");
+            var pid = $form.find(".pid").val();
+            var pname = $form.find(".pname").val();
+            var pprice = $form.find(".pprice").val();
+            var pimage = $form.find(".pimage").val();
+            var pcode = $form.find(".pcode").val();
+
+            $.ajax({
+                url:'action.php',
+                method:'post',
+                data:{pid:pid,pname:pname,pprice:pprice,pimage:pimage,pcode:pcode},
+                success:function(response){
+                    $("message").html(response);
+                }
+            });
+
+        });
+    });
+
+
+
+
+
+</script>
+
+
+
 
 </body>
 </html>
